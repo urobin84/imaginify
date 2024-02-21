1d14c4446630a19fa5a2c1714cad2b70b22ca425
Muhammad  Muqorrobin <muhammad.muqorrobin@mitracomm.com>
2024-02-21 09:58:01 +0700

    name update

--- components/shared/MediaUploader.tsx
+++ components/shared/MediaUploader.tsx
@@ -53,7 +53,7 @@ const MediaUploader = ({
 
   return (
     <CldUploadWidget
-      uploadPreset="jsm_imaginify"
+      uploadPreset="MQR_Imaginify"
       options={{
         multiple: false,
         resourceType: "image",
--- lib/actions/image.actions.ts
+++ lib/actions/image.actions.ts
@@ -113,7 +113,7 @@ export async function getAllImages({
       secure: true,
     });
 
-    let expression = "folder=imaginify";
+    let expression = "folder=MQR_Imaginify";
 
     if (searchQuery) {
       expression += ` AND ${searchQuery}`;
